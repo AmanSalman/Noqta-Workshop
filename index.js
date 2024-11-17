914ff0af4f1a6259c3065c56be03cd98795b384c
AmanSalman <rapamansalman@gmail.com>
2024-11-17 20:51:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import {products} from './products.js';
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json({message:"success", products})
+})
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
