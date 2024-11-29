5d88375e86736063fcd334a079f4e19960364820
AmanSalman <rapamansalman@gmail.com>
2024-11-29 18:52:48 +0200

    commit

--- frontendSide/main.js
+++ frontendSide/main.js
@@ -1,24 +1,28 @@
 const apiURL = "http://localhost:3000/api/products";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const productContainer = document.getElementById("product-container");
+document.addEventListener('DOMContentLoaded', ()=>{
+  const productContainer = document.getElementById('product-container')
+  console.log(productContainer)
   axios.get(apiURL).then(response => {
-      const products = response.data.products;
-      products.forEach(product => {
-        const productCard = document.createElement("div");
-        productCard.classList.add("product-card");
-        productCard.innerHTML = `
-          <img src="${product.image}" alt="${product.name}">
-          <div class="flex-dev">
-            <h2>${product.name}</h2>
-            <p class="price">Price: $${product.price}</p>
-            <button class="btn">Buy Now</button>
-          </div>
-        `;
-        productContainer.appendChild(productCard);
-      });
-    }).catch(error => {
-      console.error("Error fetching products:", error);
-      productContainer.innerHTML = `<p>Error loading products. Please try again later.</p>`;
-    });
-});
+    console.log(response)
+    const products = response.data.products;
+    products.forEach(prod =>{
+      const card = document.createElement('div')
+      card.classList.add('product-card');
+      card.innerHTML = `
+      <img src="${prod.image}" alt="${prod.name}"/>
+      <div class="flex-dev">
+      <h2>${prod.name}</h2>
+      <p class="price"> price: ${prod.price}</p>
+      <button class="btn">buy now</button>
+      </div>
+      `
+       productContainer.appendChild(card)
+    })
+   
+  }).catch(err =>{
+      console.log("error fetching data")
+      productContainer.innerHTML = `<p>error loading products , please try later</p>`
+    })
+
+})
